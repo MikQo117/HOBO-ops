e08f72256cf6b15f235284e62f4a74d1635a493a
Peltola Mikko TTK17SP <mikkopeltola@kamk.fi>
2018-04-03 18:26:32 +0300

    Add bare character class and PainterScript

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //Character stats
+    protected int     health;
+    protected int     sanity;
+    protected float   stamina;
+    protected float   staminaRecoveryRate;
+
+    //Max stats
+    protected int     maxHealth;
+    protected int     maxSanity;
+    protected int     maxStamina;
+
+    //Character movement
+    protected float   movementSpeed;
+    protected float   sprintSpeed;
+    protected Vector2 movementDirection;
+    protected bool    sprinting;
+
+    //Exhaust variables
+    protected bool    exhausted; //Must disable sprinting
+    private float     exhaustTimer;
+    protected float   exhaustDuration;
+
+    //Get & Set
+    protected int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                Death();
+            }
+            else
+            {
+                health = Mathf.Clamp(value, 0, maxHealth); 
+            }
+        }
+    }
+    protected int Sanity
+    {
+        get
+        {
+            return sanity;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                Death();
+            }
+            sanity = Mathf.Clamp(value, 0, maxSanity);
+        }
+    }
+    protected float Stamina
+    {
+        get
+        {
+            return stamina;
+        }
+
+        set
+        {
+            if (value <= 0f)
+            {
+                Exhausted();
+                stamina = 0f;
+            }
+            else
+            {
+                stamina = Mathf.Clamp(value, 0f, maxStamina);
+            }
+        }
+    }
+
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        exhaustTimer = exhaustDuration;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        GetInput();
+        RecoverStamina();
+        ExhaustTimer();
+        ApplyMovement();
+    }
+
+    protected virtual void ApplyMovement()
+    {
+        transform.Translate(movementDirection.normalized * movementSpeed * Time.deltaTime);
+    }
+
+    protected abstract void GetInput();
+    protected abstract void Death();
+
+    protected virtual void RecoverStamina()
+    {
+        if (!sprinting)
+        {
+            if (Stamina < maxStamina)
+            {
+                Stamina += staminaRecoveryRate * Time.deltaTime;  
+            }
+        }
+    }
+
+    protected virtual void Exhausted()
+    {
+        if (exhausted == false)
+        {
+            exhausted = true;
+            //Start co-routine to disable exhaustion
+            
+        }
+    }
+    protected void ExhaustTimer()
+    {
+        //Only tick when exhausted
+        if (exhausted)
+        {
+            //Still exhausted
+            if (exhaustTimer > 0f)
+            {
+                exhaustTimer -= Time.deltaTime; 
+            }
+            //Not exhausted
+            else
+            {
+                exhausted = false;
+                exhaustTimer = exhaustDuration;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/PainterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PainterScript : MonoBehaviour
+{
+
+    private List<SpriteRenderer> rend;
+    private List<GameObject>     objects;
+    private List<Vector3>        positions;
+    public bool                  sorting;
+
+    // Use this for initialization
+    void Start()
+    {
+        objects = GameObject.FindGameObjectsWithTag("Sortable").ToList();
+        /*for (int i = 0; i < objects.Count; i++)
+        {
+            positions.Add(objects[i].transform.position);
+        }
+
+        for (int i = 0; i < objects.Count; i++)
+        {
+            rend.Add(objects[i].GetComponent<SpriteRenderer>());
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sorting)
+        {
+            //Sort positions list
+            objects = objects.OrderByDescending(v => v.transform.position.y).ThenByDescending(v => v.transform.position.x).ToList();
+
+            //for through and set order in layer
+            for (int i = 0; i < objects.Count; i++)
+            {
+                objects[i].GetComponent<SpriteRenderer>().sortingOrder = i;
+            }
+        }
+
+        //Simple, right?
+    }
+}
