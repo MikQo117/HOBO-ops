cfae024829e7b4caf21642af171fe83d64eb68b9
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-04-27 15:03:14 +0300

    ADD inventory window

--- Assets/Scripts/Managers/UImanager_Player.cs
+++ Assets/Scripts/Managers/UImanager_Player.cs
@@ -9,9 +9,11 @@ public class UImanager_Player : MonoBehaviour
 
     //StatusBars variables
     private const float     maxValue = 100;
-    public Image            HealthBar;
-    public Image            SanityBar;
-    public Image            StaminaBar;
+    public  Image           HealthBar;
+    public  Image           SanityBar;
+    public  Image           StaminaBar;
+    public  Text            BottleText;
+    private GameObject      InventoryScreen;
 
     //Inventory variables
     private List<Transform> inventoryObjects = new List<Transform>();
@@ -28,7 +30,8 @@ public class UImanager_Player : MonoBehaviour
     private void Inventory()
     {
         transform.GetChild(0).gameObject.SetActive(showing);
-
+        transform.GetChild(1).gameObject.SetActive(showing);
+        BottleText.text = PlayerController.pl.CharacterInventory.InventoryList.Count(x => x.BaseItemID == 8).ToString();
         for (int i = 0; i < inventoryObjects.Count; i++)
         {
             inventoryObjects[i].GetComponentInChildren<Text>().text = PlayerController.pl.CharacterInventory.InventoryList.Count(x => x.BaseItemID == i && x != null).ToString();
@@ -45,10 +48,11 @@ public class UImanager_Player : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        for (int i = 0; i < transform.GetChild(1).childCount; i++)
         {
-            inventoryObjects.Add(transform.GetChild(0).GetChild(i).transform);
+            inventoryObjects.Add(transform.GetChild(1).GetChild(i).transform);
         }
+        InventoryScreen = transform.GetChild(1).gameObject;
     }
 
     void Update()
