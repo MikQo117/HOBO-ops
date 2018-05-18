67bfa0471a8ac6efbe3adf9816fe09a0fda3d41c
Mikko Peltola TTK17SP <mikkopeltola@kamk.fi>
2018-05-18 16:52:49 +0300

    Add Pedestrians basic movement and WIP on rush hour and night time

---
+++ Assets/Scripts/Interactables/PedestrianTarget.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianTarget : MonoBehaviour   //, IInteractable
+{
+    private void Awake()
+    {
+        GameManager.Instance.GetPedestrianTargets.Add(this);
+    }
+}
